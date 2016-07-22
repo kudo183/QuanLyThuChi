b6f7b2047243cea3be6ea59866028508e10b42b2
kudo183 <kudo183@gmail.com>
2016-07-22 12:02:13 +0700

    - add copy image in gulp script. - add header menu. - add viewport meta tag.

--- src/QuanLyThuChiWeb/gulpfile.js
+++ src/QuanLyThuChiWeb/gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding BeforeBuild='clean' AfterBuild='min' Clean='clean' />
+/// <binding BeforeBuild='clean' AfterBuild='build' Clean='clean' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -18,15 +18,24 @@ var paths = {
 
 paths.minJsDest = paths.webroot + "wwwroot/site.min.js";
 paths.minCssDest = paths.webroot + "wwwroot/site.min.css";
+paths.imageDest = paths.webroot + "wwwroot/images";
 
 //specify js files path, order is important
 paths.js = [
+    paths.webroot + "js/lib/huy.control.utilsDOM.js",
+    paths.webroot + "js/lib/huy.control.headerMenu.js",
     paths.webroot + "js/app.js"
 ];
 
 //specify cs files path, order is important
 paths.css = [
-    paths.webroot + "css/app.css"
+    paths.webroot + "css/lib/*.css",
+    paths.webroot + "css/*.css"
+];
+
+paths.image = [
+    paths.webroot + "css/images/*.*",
+    paths.webroot + "css/lib/images/*.*"
 ];
 
 gulp.task("clean:js", function (cb) {
@@ -37,7 +46,11 @@ gulp.task("clean:css", function (cb) {
     rimraf(paths.minCssDest, cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task("clean:image", function (cb) {
+    rimraf(paths.imageDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css", "clean:image"]);
 
 gulp.task("min:js", function () {
     return gulp.src(paths.js)
@@ -54,3 +67,12 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("copy:image", function () {
+    return gulp.src(paths.image)
+      .pipe(gulp.dest(paths.imageDest));
+});
+
+gulp.task("copy", ["copy:image"]);
+
+gulp.task("build", ["min", "copy"]);
--- src/QuanLyThuChiWeb/wwwroot/js/app.js
+++ src/QuanLyThuChiWeb/wwwroot/js/app.js
@@ -1,1 +1,27 @@
-﻿document.body.innerHTML = "<h1>quan ly thu chi xin chao :)</h1>";
+﻿var headerMenuViewModel = {
+    items: [
+        { text: "Item 1", value: "item1", id: "" },
+        { text: "Item 2", value: "item2", id: "" },
+        { text: "Item 3", value: "item3", id: "" }
+    ],
+    selectedItemText: ko.observable("Item 2"),
+    selectedItemValue: ko.observable("item2"),
+    buttons: [
+        {
+            text: "",
+            id: "refreshButton",
+            action: function () {
+                alert("refresh button clicked");
+            }
+        },
+        {
+            text: "A",
+            id: "aButton",
+            action: function () {
+                alert("A button clicked");
+            }
+        }
+    ]
+};
+$(document.body).append("<div id='headerMenu'/>");
+$("#headerMenu").append(window.huy.control.headerMenu.createView("headerMenuView", headerMenuViewModel));
